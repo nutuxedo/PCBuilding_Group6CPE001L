b4273f1b843a6b4b04ee078326403579e82b6adc
rj <vizconde015@gmail.com>
2024-06-04 22:43:18 +0800

    main menu ideas

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 # Start
+print("Welcome to PC Building")
+
+'''
+Main menu here
+ - add most popular pc builds
+ - create-your-pc picker
+ - login & logout system
+ - check saved builds (if logged in)
+ - exit program
+'''
