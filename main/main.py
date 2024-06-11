50ca086d1f4a4144e2e43ab29bb31f2848814784
Rafael Jose <141491411+nutuxedo@users.noreply.github.com>
2024-06-11 16:27:04 +0800

    Added main menu

--- main/main.py
+++ main/main.py
@@ -1,13 +1,47 @@
 # Start
-print("Welcome to PC Building")
-
 # Ideas here:
 # Check document for components and prices for reference
 '''
 Main menu here
  - add most popular pc builds
  - create-your-pc picker
- - login & logout system
- - check saved builds (if logged in)
+ - credentials
+ - track your order (from receipt)
  - exit program
 '''
+
+# Main menu class
+
+class MainMenu():
+    def __init__(self): # Main menu
+        while True:
+            operation = input('''
+Select operation:
+[1] Create your PC
+[2] Track your order
+[3] Saved builds
+[4] Exit program
+
+Operation: ''')
+            if operation == '1':
+                print("You have selected choice 1")
+
+            elif operation == '2':
+                print("You have selected choice 2")
+
+            elif operation == '3':
+                print("You have selected choice 3")
+
+            elif operation == '4':
+                break
+
+            else:
+                print("Invalid choice. Please try again.")
+
+# Code initialization
+def main():
+    menu = MainMenu()
+
+if __name__ == "__main__":
+    main()
+
